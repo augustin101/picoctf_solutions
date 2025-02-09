d86e9395720772143d78970881ab61b340d1782b
Augustin <augustin.henry@freshape.com>
2025-02-09 16:13:28 +0100

    feat: solution credstuff

---
+++ medium/cryptography/credstuff/credstuff.py
@@ -0,0 +1,5 @@
+import codecs
+usernames = open("usernames.txt").read().splitlines()
+passwords = open("passwords.txt").read().splitlines()
+idx = usernames.index("cultiris")
+print(codecs.decode(passwords[idx], "rot_13"))
