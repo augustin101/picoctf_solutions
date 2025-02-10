9d65db0b56e92efd46016d8c4b306aa6491c0d9b
Augustin <augustin.henry@freshape.com>
2025-02-10 22:56:17 +0100

    feat: solution for basic-mod1

---
+++ medium/cryptography/basic-mod1.py
@@ -0,0 +1,4 @@
+inp = list(map(int,"165 248 94 346 299 73 198 221 313 137 205 87 336 110 186 69 223 213 216 216 177 138".split()))
+moded = [e % 37 for e in inp]
+converted = [chr(ord("A") + e) if e < 26 else str(e - 26) if e < 36 else "_" for e in moded]
+print(f"picoCTF{{{"".join(converted)}}}")
